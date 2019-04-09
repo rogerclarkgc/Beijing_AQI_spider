1046d3173e9a9948c4a2b16ca71227af4fb22d5b
rogerclarkgc <rogerclark@163.com>
2019-04-09 17:18:39 +0800

    ADD[roger]: add screenshot modules for spider

---
+++ basicset/selector.py
@@ -0,0 +1,21 @@
+# define css selector for pollutants buttons
+
+AQI = "body > div.btns > ul > li:nth-child(1) > a"
+PM25 = "body > div.btns > ul > li:nth-child(2) > a"
+SO2 = "body > div.btns > ul > li:nth-child(3) > a"
+NO2 = "body > div.btns > ul > li:nth-child(4) > a"
+O3 = "body > div.btns > ul > li:nth-child(5) > a"
+CO = "body > div.btns > ul > li:nth-child(6) > a"
+PM10 = "body > div.btns > ul > li:nth-child(7) > a"
+
+PULLUTANTS = {'AQI': AQI,
+             'PM25': PM25,
+             'SO2': SO2,
+             'NO2': NO2,
+             'O3': O3,
+             'CO': CO,
+             "PM10": PM10}
+
+test1 = "#t_overlaysDiv > div:nth-child(30) > div > div > img"
+test2 = "#t_overlaysDiv > div:nth-child(35) > div > div > img"
+test3 = "#t_overlaysDiv > div:nth-child(36) > div > div > img"
---
+++ get_screen/bjmemc.py
@@ -0,0 +1,65 @@
+# coding:utf-8
+# Author:rogerclark
+
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import  ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.common import exceptions
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from basicset import selector
+
+class Screenshotbjmemc(object):
+
+    def __init__(self, base_url, wait=20, retry=3):
+
+        print("Initiating Chromedriver...")
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.retry = retry
+        while self.retry > 0:
+            try:
+                self.driver.get(base_url)
+                WebDriverWait(self.driver, wait).until(EC.visibility_of_element_located((By.TAG_NAME, 'svg')))
+                break
+            except exceptions.TimeoutException:
+                print("Failed to load the page, retry in 5 seconds...")
+                self.retry -= 1
+                time.sleep(5)
+        if self.retry <= 0:
+            raise RuntimeError("Spider has reached the max retry times, page get failed.")
+
+
+
+    def switchFrameButton(self, selector, wait=5):
+
+        switch = ActionChains(self.driver)
+        try:
+            button = self.driver.find_element_by_css_selector(selector)
+            switch.move_to_element(button).click().perform()
+            time.sleep(wait)
+        except exceptions.NoSuchElementException:
+            raise RuntimeError("Can not find the element in page")
+
+
+    def captureScreen(self, filename):
+
+        self.driver.get_screenshot_as_file(filename)
+
+    def closeDriver(self):
+
+        self.driver.close()
+
+
+if __name__ == "__main__":
+
+    base_url = "http://zx.bjmemc.com.cn/getAqiList.shtml?timestamp="
+    sel = selector.PM10
+    bj = Screenshotbjmemc(base_url=base_url)
+    bj.switchFrameButton(sel, wait=5)
+    bj.captureScreen("pm10.png")
+    bj.closeDriver()
